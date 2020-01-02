c9e34e99e2904621f736c8cb33e7f3d6c255684e
bobacCurry <bobac.curry@gmail.com>
2020-01-02 10:48:25 +0800

    fix

---
+++ addJob.py
@@ -0,0 +1,62 @@
+from model.Push import Push
+
+from model.Queue import Queue
+
+from model.AddChat import AddChat
+
+from model.Client import Client
+
+import logging
+
+import datetime
+
+import time
+
+import sys
+
+def logger(info):
+	
+	today=datetime.date.today()
+
+	filename = 'log/'+'cron'+'-'+str(today)+'.log'
+
+	logging.basicConfig(level=logging.INFO,filename=filename,format='%(asctime)s - %(message)s')
+	
+	logging.info(info)
+
+push_obj = Push()
+
+queue_obj = Queue()
+
+now = datetime.datetime.now()
+
+minute = now.minute
+
+minute1 = minute + 2
+	
+if minute==58:
+	
+	minute1 = 0
+
+if minute==59:
+	
+	minute1 = 1
+
+# 将群分割
+slice_num = 30
+
+pushs1 = push_obj.find({"minute":minute,"message_id":{"$ne":0},"status":1},{"phone":1,"chat":{"$slice":slice_num},"message_id":1,"text_type":1,"text":1,"media":1,"caption":1})
+
+pushs2 = push_obj.find({"minute":minute1,"message_id":{"$ne":0},"count":{"$gt":slice_num*1},"status":1},{"phone":1,"chat":{"$slice":[slice_num*1,slice_num]},"message_id":1,"text_type":1,"text":1,"media":1,"caption":1})
+
+pushs = pushs1 + pushs2
+
+# pushs = self.push_obj.find({"minute":minute,"message_id":{"$ne":0},"status":1},{"phone":1,"chat":1,"message_id":1})
+
+for push in pushs:
+
+	queue_obj.insert({"phone":push["phone"],"chat":push["chat"],"message_id":push["message_id"]})
+
+logger(str(minute) + '分任务添加')
+
+sys.exit()
---
+++ clearJob.py
@@ -0,0 +1,73 @@
+from cronrunner.index import Index
+
+from model.Queue import Queue
+
+from model.Client import Client
+
+from model.Queue import Queue
+
+from model.Push import Push
+
+import datetime
+
+import time
+
+import logging
+
+from client.message import Message
+
+import sys
+
+client_obj = Client()
+
+queue_obj = Queue()
+
+push_obj = Push()
+
+def logger(info):
+	
+	today=datetime.date.today()
+
+	filename = 'log/'+'cron'+'-'+str(today)+'.log'
+
+	logging.basicConfig(level=logging.INFO,filename=filename,format='%(asctime)s - %(message)s')
+	
+	logging.info(info)
+
+def forward(phone,chatids,message_id):
+	
+	message = Message(phone)
+
+	log = str(phone)
+
+	for chatid in chatids:
+
+		ret = message.forward_message(chatid,'me',message_id)
+
+		if ret['success']:
+			
+			log = log + '-' + chatid + '（success）'
+
+		else:
+
+			log = log + '-' + chatid + '（' + ret['msg'] + '）'
+
+			if 'check @SpamBot' in ret['msg']:
+				
+				client_obj.update({'phone':phone},{'status':2})
+
+				push_obj.update({'phone':phone},{'status':0})
+
+				break
+
+	logger(log)
+
+queue = queue_obj.findOne({})
+
+if queue:
+
+	forward(queue["phone"],queue["chat"],queue["message_id"])
+	
+	queue_obj.remove({"_id":queue["_id"]})
+
+sys.exit()
