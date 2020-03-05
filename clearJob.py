eca5cec6d89ca27893146477c95d1e10fe188bf6
bobacCurry <bobac.curry@gmail.com>
2020-03-05 18:29:14 +0800

    fix

--- clearJob.py
+++ clearJob.py
@@ -18,14 +18,15 @@ from client.message import Message
 
 import sys
 
+import signal
+
+import time
+
 client_obj = Client()
 
 queue_obj = Queue()
 
 push_obj = Push()
-
-
-
 
 def logger(info):
 	
@@ -39,6 +40,45 @@ def logger(info):
 
 	return
 
+def set_timeout(num, callback):
+
+    def wrap(func):
+    
+        def handle(signum, frame):  # 收到信号 SIGALRM 后的回调函数，第一个参数是信号的数字，第二个参数是the interrupted stack frame.
+    
+            raise RuntimeError
+    
+        def to_do(*args, **kwargs):
+    
+            try:
+    
+                signal.signal(signal.SIGALRM, handle)  # 设置信号和回调函数
+    
+                signal.alarm(num)  # 设置 num 秒的闹钟
+    
+                print('start alarm signal.')
+    
+                r = func(*args, **kwargs)
+    
+                print('close alarm signal.')
+    
+                signal.alarm(0)  # 关闭闹钟
+    
+                return r
+    
+            except RuntimeError as e:
+    
+                callback()
+    
+        return to_do
+    
+    return wrap
+
+def after_timeout():  # 超时后的处理函数
+    
+    logger('--------超时退出--------')
+
+@set_timeout(60, after_timeout)  # 限时 60 秒超时
 def forward(phone,chatids,message_id):
 	
 	message = Message(phone)
